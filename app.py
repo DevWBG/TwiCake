a736530ba87ffd3184ffc9c48f10281b6434b27d
DevWBG <DevWBG@gmail.com>
2014-08-12 15:10:24 +0900

    message

--- app.py
+++ app.py
@@ -9,13 +9,7 @@ from tweepy.auth import OAuthHandler
 from tweepy.api import API
 
 app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-#config of App
-app.config.update(
-    consumer_key='o7vgHO8hRGPl62L9xokp76Fan',
-    consumer_secret='DuBOE39WluGJeRdUtz3sXAN7pjPr4mtoaRJYe5IueIRaeOejID'
-)
 
 #setting SQLAlchemy engine
 engine = create_engine('sqlite:///:memory', echo=True)
@@ -25,23 +19,24 @@ session = session_class()
 class myExeption(Exception):
     pass
 
+
 class StreamListener(tweepy.streaming.StreamListener):
     tweet_id = None
     user_list = []
 
     def __init__(self, send_tweet_id):
         super(StreamListener, self).__init__()
-        self.tweet_id = send_tweet_id 
+        self.tweet_id = send_tweet_id
 
     def on_status(self, status):
         if hasattr(status, 'retweeted_status'):
-            retweeted_id =  status.retweeted_status.id
+            retweeted_id = status.retweeted_status.id
             retweeted_count = status.retweeted_status.retweet_count
             retweeted_user = status.author
             user_name = retweeted_user.screen_name
             if self.tweet_id == retweeted_id:
                 print user_name
-                self.user_list.append(user_name) 
+                self.user_list.append(user_name)
 
     def on_error(self, status):
         print "can't get"
@@ -88,29 +83,35 @@ class Status(User):
         User.__init__(self, user_id, screen_id)
         self.status_id = status_id
 
-
 @app.route('/')
 def my_render():
     if 'access_key' in session:
         return render_template('twicake.html', access_status=True)
     else:
-        new_user = Status(
         return render_template('twicake.html', access_status=False)
+    return render_template(
+        'twicake.html',
+        access_status=('access_key' in session)
+        )
 
 
 @app.route('/auth')
 def auth():
     callback_url = url_for('get_auth', _external=True)
-    auth = tweepy.OAuthHandler(app.config['consumer_key'],
-            app.config['consumer_secret'], callback_url)
+    auth = tweepy.OAuthHandler(
+        app.config['consumer_key'],
+        app.config['consumer_secret'],
+        callback_url)
     try:
         redirect_url = auth.get_authorization_url()
         app.config['request_key'] = auth.request_token.key
         app.config['request_secret'] = auth.request_token.secret
         return redirect(redirect_url)
     except tweepy.TweepError as e:
-        return render_template('error_auth.html',
-                error_message=e.message), 401
+        return (render_template(
+                'error_auth.html',
+                error_message=e.message),
+                401)
 
 
 @app.route('/', methods=['POST'])
@@ -118,18 +119,22 @@ def my_send_post():
     if 'tweepy_api' in app.config:
         api = app.config['tweepy_api']
     elif 'access_secret' in session:
-        auth = tweepy.OAuthHandler(app.config['consumer_key'],
-                app.config['consumer_secret'])
-        auth.set_access_token(session['access_key'],
-                session['access_secret'])
+        auth = tweepy.OAuthHandler(
+            app.config['consumer_key'],
+            app.config['consumer_secret'])
+        auth.set_access_token(
+            session['access_key'],
+            session['access_secret'])
         api = tweepy.API(auth)
     else:
         return redirect(url_for('auth'))
     try:
         api.update_status(request.form['send_text'])
     except tweepy.TweepError as e:
-        return render_template('error_auth.html',
-                error_message=e.message), 401
+        return (render_template(
+            'error_auth.html',
+            error_message=e.message),
+            401)
 
     return 'successful send : ' + request.form['send_text']
 
@@ -138,10 +143,12 @@ def my_send_post():
 def get_auth():
     if 'oauth_verifier' in request.args:
         verifier = request.args['oauth_verifier']
-        auth = tweepy.OAuthHandler(app.config['consumer_key'],
-                app.config['consumer_secret'])
-        auth.set_request_token(app.config['request_key'],
-                app.config['request_secret'])
+        auth = tweepy.OAuthHandler(
+            app.config['consumer_key'],
+            app.config['consumer_secret'])
+        auth.set_request_token(
+            app.config['request_key'],
+            app.config['request_secret'])
         try:
             auth.get_access_token(verifier)
             session['access_key'] = auth.access_token.key
@@ -150,8 +157,10 @@ def get_auth():
             session['request_secret'] = app.config['request_secret']
             app.config['tweepy_api'] = tweepy.API(auth)
         except tweepy.TweepError as e:
-            return render_template('error_auth.html',
-                    error_message=e.message), 401
+            return (render_template(
+                    'error_auth.html',
+                    error_message=e.message),
+                    401)
         return redirect(url_for('my_render'))
 
     else:
